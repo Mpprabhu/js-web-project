dd60d07385c303ca003ebc7ff897c63883a53801
Mpprabhu <prabhu92004@gmail.com>
2024-07-05 01:55:39 +0530

    Day 31 reveal elements design

--- script.js
+++ script.js
@@ -124,3 +124,38 @@ tabContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+//Sticky Navbar
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+// Reveal Animation
+const revealELement = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const observer = new IntersectionObserver(revealELement, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  observer.observe(section);
+  section.classList.add('section--hidden');
+});
